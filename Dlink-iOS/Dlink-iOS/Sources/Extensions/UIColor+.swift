7aa755627b9decdc5f9e6c63ed894c4d92a839a3
Noah <noa316@naver.com>
2021-11-21 01:41:30 +0900

    ➕[ADD] UIColor값 추가

---
+++ Dlink-iOS/Dlink-iOS/Sources/Extensions/UIColor+.swift
@@ -0,0 +1,79 @@
+//
+//  UIColor+.swift
+//  Dlink-iOS
+//
+//  Created by Noah on 2021/11/21.
+//
+
+import UIKit
+
+extension UIColor {
+	
+	@nonobjc class var bgStartColor: UIColor {
+		return UIColor(red: 8 / 255, green: 12 / 255, blue: 34 / 255, alpha: 1)
+	}
+	
+	@nonobjc class var bgEndColor: UIColor {
+		return UIColor(red: 25 / 255, green: 38 / 255, blue: 76 / 255, alpha: 1)
+	}
+	
+	@nonobjc class var soju: UIColor {
+		return UIColor(red: 65.0 / 255.0, green: 209.0 / 255.0, blue: 140.0 / 255.0, alpha: 1.0)
+	}
+	
+	@nonobjc class var etc: UIColor {
+		return UIColor(red: 1.0, green: 158.0 / 255.0, blue: 198.0 / 255.0, alpha: 1.0)
+	}
+	
+	@nonobjc class var beerSub: UIColor {
+		return UIColor(red: 101.0 / 255.0, green: 63.0 / 255.0, blue: 28.0 / 255.0, alpha: 1.0)
+	}
+	
+	@nonobjc class var beer: UIColor {
+		return UIColor(red: 1.0, green: 225.0 / 255.0, blue: 118.0 / 255.0, alpha: 1.0)
+	}
+	
+	@nonobjc class var macSub: UIColor {
+		return UIColor(red: 194.0 / 255.0, green: 144.0 / 255.0, blue: 47.0 / 255.0, alpha: 1.0)
+	}
+	
+	@nonobjc class var mac: UIColor {
+		return UIColor(red: 1.0, green: 250.0 / 255.0, blue: 224.0 / 255.0, alpha: 1.0)
+	}
+	
+	@nonobjc class var etcSub: UIColor {
+		return UIColor(red: 1.0, green: 112.0 / 255.0, blue: 81.0 / 255.0, alpha: 1.0)
+	}
+	
+	@nonobjc class var sojuSub: UIColor {
+		return UIColor(red: 193.0 / 255.0, green: 244.0 / 255.0, blue: 1.0, alpha: 1.0)
+	}
+	
+	@nonobjc class var white: UIColor {
+		return UIColor(white: 1.0, alpha: 1.0)
+	}
+	
+	@nonobjc class var navySub2: UIColor {
+		return UIColor(red: 37.0 / 255.0, green: 53.0 / 255.0, blue: 98.0 / 255.0, alpha: 1.0)
+	}
+	
+	@nonobjc class var navy: UIColor {
+		return UIColor(red: 25.0 / 255.0, green: 38.0 / 255.0, blue: 76.0 / 255.0, alpha: 1.0)
+	}
+	
+	@nonobjc class var navySub250: UIColor {
+		return UIColor(red: 37.0 / 255.0, green: 53.0 / 255.0, blue: 98.0 / 255.0, alpha: 0.5)
+	}
+	
+	@nonobjc class var navySub3: UIColor {
+		return UIColor(red: 61.0 / 255.0, green: 79.0 / 255.0, blue: 141.0 / 255.0, alpha: 1.0)
+	}
+	
+	@nonobjc class var navySub4: UIColor {
+		return UIColor(red: 12.0 / 255.0, green: 21.0 / 255.0, blue: 53.0 / 255.0, alpha: 1.0)
+	}
+	
+	@nonobjc class var navySub5: UIColor {
+		return UIColor(red: 105.0 / 255.0, green: 123.0 / 255.0, blue: 184.0 / 255.0, alpha: 1.0)
+	}
+}
