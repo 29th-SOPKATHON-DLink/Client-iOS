f5b8e4eb1272adf81984b5a6d0f1a76b5a31ebc6
so yeon <alicesysy@swu.ac.kr>
2021-11-21 01:46:29 +0900

    ✅[CHORE] 스토리보드로 UI 구현

---
+++ Dlink-iOS/Dlink-iOS/Sources/ViewControllers/CreateGroupVC.swift
@@ -0,0 +1,17 @@
+//
+//  CreateGroupVC.swift
+//  Dlink-iOS
+//
+//  Created by soyeon on 2021/11/21.
+//
+
+import UIKit
+
+class CreateGroupVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
